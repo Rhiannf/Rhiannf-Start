6b7b0ad23de720ec6e21cb87df2264d3dfb1add0
RhiannF <rhiannf04@gmail.com>
2023-02-27 16:05:04 +0000

    Start

--- src/main/java/GuideMain.java
+++ src/main/java/GuideMain.java
@@ -30,7 +30,7 @@ public class GuideMain {
     }
 }
 /**
- *  Now, locate the test class and run its test. It should pass, and your IDE should inform you about the results
+ *  Now, locate that test class and run its test. It should pass, and your IDE should inform you about the results
  *  of the tests.
  *  Your is will be modifying the code in the remainder of the course repo so that their test cases pass rather
  *  than fail.
